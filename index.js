532cadafedff36d0eff258920664f8173643135c
sk <sk.paradoxmaske@gmail.com>
2021-05-26 12:21:51 -0500

    redo nodejs routing with express

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
 
 
-http.createServer(function (req, res){
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
+// http.createServer(function (req, res){
+//     res.writeHead(200, {
+//         'Content-Type': 'text/html'
+//     });
 
-    let path = "";
-    switch(req.url){
-        case '/':
-            path = './index.html';
-            break;
-        case '/about':
-            path = './about.html';
-            break;
-        case '/contact-me':
-            path = './contact-me.html';
-            break;
-        default:
-            path = './404.html';
-            break;
-    }
+//     let path = "";
+//     switch(req.url){
+//         case '/':
+//             path = './index.html';
+//             break;
+//         case '/about':
+//             path = './about.html';
+//             break;
+//         case '/contact-me':
+//             path = './contact-me.html';
+//             break;
+//         default:
+//             path = './404.html';
+//             break;
+//     }
 
-    fs.readFile(path, (err, data) =>{
-        if(err){
-            console.log(err);
-            res.end();
-        }
-        else{
-            res.end(data);
-        }
-    });
+//     fs.readFile(path, (err, data) =>{
+//         if(err){
+//             console.log(err);
+//             res.end();
+//         }
+//         else{
+//             res.end(data);
+//         }
+//     });
 
-}).listen(8080);
+// }).listen(8080);
+
+
+//express
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+app.get('/about', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './about.html'));
+});
+
+app.get('/contact-me', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './contact-me.html'));
+});
+
+app.all('*', (req, res) =>{
+    res.status(404).sendFile(path.resolve(__dirname, './404.html'));
+});
+
+app.listen(8080);
