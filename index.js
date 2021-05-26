1e7935fa03335972375d6cf1eed349255a3b4208
sk <sk.paradoxmaske@gmail.com>
2021-05-26 12:33:46 -0500

    fix link paths

--- index.js
+++ index.js
@@ -36,10 +36,11 @@
 // }).listen(8080);
 
 
-//express
+// express
 const express = require('express');
 const app = express();
 const path = require('path');
+
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './index.html'));
